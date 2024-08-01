67b412d5079d15bce435134b5f7031effc3d928b
ck <ck@ck.local>
2024-08-01 09:42:25 +0530

    maven

--- infra/main.tf
+++ infra/main.tf
@@ -32,8 +32,10 @@ resource "aws_instance" "jenkins" {
   sudo apt-get install git wget unzip curl tree -y
   sudo apt-get -y install git binutils
   sudo apt-get install openjdk-17-jdk -y
+  sudo apt-get install maven -y
   sudo cp -pvr /etc/environment "/etc/environment_$(date +%F_%R)"
   echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/" >> /etc/environment
+  echo "MAVEN_HOME=/usr/share/maven" >> /etc/environment
   source /etc/environment
   sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
   echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
