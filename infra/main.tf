afa96b2c0376930063d7d8fb9b0c4b8dd35e6dec
ck <ck@ck.local>
2024-08-02 09:38:24 +0530

    jfrog

--- infra/main.tf
+++ infra/main.tf
@@ -104,6 +104,68 @@ resource "aws_instance" "sonarqube" {
   }
 }
 
+
+# Resource
+resource "aws_instance" "jfrog" {
+  ami                    = var.ami
+  instance_type          = var.sonar_instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id_1a
+  vpc_security_group_ids = ["sg-0a3fe1b73a2b3e0d2"]
+  iam_instance_profile   = var.iam_instance_profile
+  #user_data              = file("/Users/ck/repos/iac-terraform/iac/web.sh")
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo hostnamectl set-hostname "jfrog.cloudbinary.io"
+  echo "`hostname -I | awk '{ print $1}'` `hostname`" >> /etc/hosts
+  sudo apt-get update
+  sudo apt-get install vim curl elinks unzip wget tree git -y
+  sudo apt-get install openjdk-17-jdk -y
+  sudo cp -pvr /etc/environment "/etc/environment_$(date +%F_%R)"
+  echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/" >> /etc/environment
+  source /etc/environment
+  cd /opt/
+  sudo wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/[RELEASE]/jfrog-artifactory-oss-[RELEASE]-linux.tar.gz
+  tar xvzf jfrog-artifactory-oss-\[RELEASE\]-linux.tar.gz
+  mv artifactory-oss-* jfrog
+  sudo cp -pvr /etc/environment "/etc/environment_$(date +%F_%R)"
+  echo "JFROG_HOME=/opt/jfrog" >> /etc/environment
+  #cd /opt/jfrog/app/bin/
+  #./artifactory.sh status
+  
+  # Configure INIT Scripts for JFrog Artifactory
+  # sudo vi /etc/systemd/system/artifactory.service
+
+  echo "[Unit]" > /etc/systemd/system/artifactory.service
+  echo "Description=JFrog artifactory service" >> /etc/systemd/system/artifactory.service
+  echo "After=syslog.target network.target" >> /etc/systemd/system/artifactory.service
+  echo "[Service]" >> /etc/systemd/system/artifactory.service
+  echo "Type=forking" >> /etc/systemd/system/artifactory.service
+  echo "ExecStart=/opt/jfrog/app/bin/artifactory.sh start" >> /etc/systemd/system/artifactory.service
+  echo "ExecStop=/opt/jfrog/app/bin/artifactory.sh stop" >> /etc/systemd/system/artifactory.service
+  echo "User=root" >> /etc/systemd/system/artifactory.service
+  echo "Group=root" >> /etc/systemd/system/artifactory.service 
+  echo "Restart=always" >> /etc/systemd/system/artifactory.service
+  echo "[Install]" >> /etc/systemd/system/artifactory.service
+  echo "WantedBy=multi-user.target" >> /etc/systemd/system/artifactory.service
+
+  sudo systemctl daemon-reload
+  sudo systemctl enable artifactory.service
+  sudo systemctl restart artifactory.service
+
+  #Sonar admin & admin | Jfrog admin & password 
+
+  EOF
+
+  tags = {
+    Name        = "JFrog"
+    Environment = "Dev"
+    ProjectName = "Cloud Binary"
+    ProjectID   = "2024"
+    CreatedBy   = "IaC Terraform"
+  }
+}
+
 # # Resource
 # resource "aws_instance" "efs2" {
 #   ami                    = var.ami
@@ -147,6 +209,21 @@ output "public_ip" {
 output "private_ip" {
   value = aws_instance.jenkins.private_ip
 }
+
+output "sonar_public_ip" {
+  value = aws_instance.sonarqube.public_ip
+}
+output "sonar_private_ip" {
+  value = aws_instance.sonarqube.private_ip
+}
+
+output "jfrog_public_ip" {
+  value = aws_instance.jfrog.public_ip
+}
+output "jfrog_private_ip" {
+  value = aws_instance.jfrog.private_ip
+}
+
 
 # output "ami_ec2" {
 #   value = aws_instance.efs2.ami
