c7771a702259adcd8ad0de614f36cfb843c767df
ck <ck@ck.local>
2024-08-05 09:50:57 +0530

    infra

--- infra/main.tf
+++ infra/main.tf
@@ -125,8 +125,9 @@ resource "aws_instance" "jfrog" {
   echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/" >> /etc/environment
   source /etc/environment
   cd /opt/
-  sudo wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/[RELEASE]/jfrog-artifactory-oss-[RELEASE]-linux.tar.gz
-  tar xvzf jfrog-artifactory-oss-\[RELEASE\]-linux.tar.gz
+  #sudo wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/[RELEASE]/jfrog-artifactory-oss-[RELEASE]-linux.tar.gz
+  sudo wget https://releases.jfrog.io/artifactory/bintray-artifactory/org/artifactory/oss/jfrog-artifactory-oss/7.71.3/jfrog-artifactory-oss-7.71.3-linux.tar.gz
+  tar xvzf jfrog-artifactory-oss-7.71.3-linux.tar.gz
   mv artifactory-oss-* jfrog
   sudo cp -pvr /etc/environment "/etc/environment_$(date +%F_%R)"
   echo "JFROG_HOME=/opt/jfrog" >> /etc/environment
@@ -165,6 +166,61 @@ resource "aws_instance" "jfrog" {
     CreatedBy   = "IaC Terraform"
   }
 }
+
+
+# Resource
+resource "aws_instance" "tomcat" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id_1a
+  vpc_security_group_ids = ["sg-0a3fe1b73a2b3e0d2"]
+  iam_instance_profile   = var.iam_instance_profile
+  #user_data              = file("/Users/ck/repos/iac-terraform/iac/web.sh")
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo hostnamectl set-hostname "tomcat.cloudbinary.io"
+  echo "`hostname -I | awk '{ print $1}'` `hostname`" >> /etc/hosts
+  sudo apt-get update
+  sudo apt-get install vim curl elinks unzip wget tree git -y
+  sudo apt-get install openjdk-17-jdk -y
+  sudo cp -pvr /etc/environment "/etc/environment_$(date +%F_%R)"
+  echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/" >> /etc/environment
+  source /etc/environment
+  cd /opt/
+  sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.91/bin/apache-tomcat-9.0.91.tar.gz
+  tar xvzf apache-tomcat-9.0.91.tar.gz
+  mv apache-tomcat-9.0.91 tomcat
+  sudo cp -pvr /opt/tomcat/conf/tomcat-users.xml "/opt/tomcat/conf/tomcat-users.xml_$(date +%F_%R)"
+  sed -i '$d' /opt/tomcat/conf/tomcat-users.xml
+
+  echo '<role rolename="manager-gui"/>'  >> /opt/tomcat/conf/tomcat-users.xml
+  echo '<role rolename="manager-script"/>' >> /opt/tomcat/conf/tomcat-users.xml
+  echo '<role rolename="manager-jmx"/>'    >> /opt/tomcat/conf/tomcat-users.xml
+  echo '<role rolename="manager-status"/>' >> /opt/tomcat/conf/tomcat-users.xml
+  
+  echo '<role rolename="admin-gui"/>'     >> /opt/tomcat/conf/tomcat-users.xml
+  echo '<role rolename="admin-script"/>' >> /opt/tomcat/conf/tomcat-users.xml
+
+  echo '<user username="admin" password="redhat@123" roles="manager-gui,manager-script,manager-jmx,manager-status,admin-gui,admin-script"/>' >> /opt/tomcat/conf/tomcat-users.xml
+  
+  echo "</tomcat-users>" >> /opt/tomcat/conf/tomcat-users.xml
+
+  cd /opt/tomcat/bin/
+
+  ./startup.sh
+
+  EOF
+
+  tags = {
+    Name        = "tomcat"
+    Environment = "Dev"
+    ProjectName = "Cloud Binary"
+    ProjectID   = "2024"
+    CreatedBy   = "IaC Terraform"
+  }
+}
+
 
 # # Resource
 # resource "aws_instance" "efs2" {
